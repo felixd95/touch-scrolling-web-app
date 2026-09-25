7310be63743db1fd2d8914efbfbfc7bb9d537fbf
felixd95 <felix.dietrich-95@outlook.de>
2026-09-25 11:22:33 +0200

    bounds

--- src/scrollPhysics/overScrollerPhysics.js
+++ src/scrollPhysics/overScrollerPhysics.js
@@ -25,15 +25,15 @@ export const FLING_PHYSICS_CONFIG = {
 
 export const FLING_PHYSICS_BOUNDS = {
   scrollFriction: {
-    min: 0.005,
+    min: 0.001,
     max: 0.1,
   },
   decelerationRate: {
-    min: 1.2,
+    min: 1.0,
     max: 5.0,
   },
   inflexion: {
-    min: 0.15,
+    min: 0.01,
     max: 1.0,
   },
 };
